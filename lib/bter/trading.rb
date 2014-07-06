ca8c4a04ae8c3821c5a452ac089da3d4b337692e
Zisis Maras <zisismaras@gmail.com>
2014-07-06 20:25:27 +0300

    trade api refactor

--- lib/bter/trading.rb
+++ lib/bter/trading.rb
@@ -6,27 +6,25 @@ module Bter
   
     attr_accessor :key, :secret
     
-    METHODS = ['getfunds', 'orderlist', 'getorder', 'cancelorder', 'mytrades'] 
+    METHODS = ['getfunds', 'orderlist', 'getorder', 'cancelorder', 'mytrades', 'placeorder'] 
 
     METHODS.each do |method|
       define_method(method) do |param=nil|
-        if param.to_i > 0
+        if param.is_a? Hash
+          trade_request(method, [param])
+        elsif param.to_i > 0
           trade_request(method, [{:order_id => param}])
         else
           trade_request(method, [{:pair => param}])
         end
       end  
     end
-
-    #abstract the trade to buy and sell
-    def buy(pair, amount, rate=nil)
-      rate ||= get_rate(pair)
-      trade({:pair => pair, :type => "BUY", :rate => rate, :amount => amount})
-    end
     
-    def sell(pair, amount, rate=nil)
-      rate ||= get_rate(pair)
-      trade({:pair => pair, :type => "SELL", :rate => rate, :amount => amount})
+    ['buy', 'sell'].each do |method|
+      define_method(method) do |pair, amount, rate=nil|
+        rate ||= get_rate(pair)
+        trade({:pair => pair, :type => method.upcase, :rate => rate, :amount => amount})
+      end
     end
    
     alias_method :get_info, :getfunds 
@@ -34,16 +32,12 @@ module Bter
     alias_method :order_status, :getorder
     alias_method :cancel_order, :cancelorder
     alias_method :my_trades, :mytrades    
+    alias_method :trade, :placeorder   
 
     private
-    def trade(*params)
-      query = trade_request("placeorder", params)
-    end
-
     def get_rate(pair)
       Public.new.ticker(pair).values_at(:last).flatten.first
-    end
-    
+    end   
   end
 end
      
